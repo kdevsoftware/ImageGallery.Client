11ccd49b538aa5f0b74b73de31f4e1eaf0010fbb
Alexey Borodenko <kosunix@gmail.com>
2018-12-28 14:27:55 +0200

    Fix unit test GetAlbums_ReturnsData.

--- test/ImageGallery.Client.Test/Helpers/WebTestHelpers.cs
+++ test/ImageGallery.Client.Test/Helpers/WebTestHelpers.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -36,9 +38,21 @@ namespace ImageGallery.Client.Test.Helpers
         {
             var principal = GetClaimsPrincipal();
 
+            var authenticationServiceMock = new Mock<IAuthenticationService>();
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock.Setup(provider => provider.GetService(typeof(IAuthenticationService)))
+                .Returns(authenticationServiceMock.Object);
+
+            var response = new Mock<HttpResponse>();
+            response.Setup(x => x.Headers).Returns(new Mock<IHeaderDictionary>().Object);
+
             var contextMock = new Mock<HttpContext>();
             contextMock.SetupGet(ctx => ctx.User)
                 .Returns(principal);
+            contextMock.SetupGet(ctx => ctx.RequestServices)
+                .Returns(serviceProviderMock.Object);
+            contextMock.SetupGet(x => x.Response).Returns(response.Object);
             var controllerContextMock = new Mock<ControllerContext>();
             var controllerContext = controllerContextMock.Object;
             controllerContext.HttpContext = contextMock.Object;
