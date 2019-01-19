dc0b2499ff6303daf660c8f15909ebdc63535e5d
Stuart Shay <sshay@yahoo.com>
2019-01-19 08:36:10 -0500

    Project Cleanup

--- src/ImageGallery.Client/wwwroot/app/shared/shared.module.ts
+++ src/ImageGallery.Client/wwwroot/app/shared/shared.module.ts
@@ -7,7 +7,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
@@ -15,10 +14,8 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { RatingModule } from 'ngx-bootstrap/rating';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
-import { DatepickerModule } from 'ngx-bootstrap/datepicker';
 
 
 import { ColorsService } from './colors/colors.service';
@@ -27,7 +24,6 @@ import { NowDirective } from './directives/now/now.directive';
 import { ScrollableDirective } from './directives/scrollable/scrollable.directive';
 
 
-// https://angular.io/styleguide#!#04-10
 @NgModule({
     imports: [
         CommonModule,
@@ -37,16 +33,13 @@ import { ScrollableDirective } from './directives/scrollable/scrollable.directiv
         AccordionModule.forRoot(),
         AlertModule.forRoot(),
         ButtonsModule.forRoot(),
-        CarouselModule.forRoot(),
         CollapseModule.forRoot(),
-        DatepickerModule.forRoot(),
         BsDropdownModule.forRoot(),
         ModalModule.forRoot(),
         PaginationModule.forRoot(),
         ProgressbarModule.forRoot(),
         RatingModule.forRoot(),
         TabsModule.forRoot(),
-        TimepickerModule.forRoot(),
         TooltipModule.forRoot(),
         TypeaheadModule.forRoot()
     ],
@@ -67,16 +60,13 @@ import { ScrollableDirective } from './directives/scrollable/scrollable.directiv
         AccordionModule,
         AlertModule,
         ButtonsModule,
-        CarouselModule,
         CollapseModule,
-        DatepickerModule,
         BsDropdownModule,
         ModalModule,
         PaginationModule,
         ProgressbarModule,
         RatingModule,
         TabsModule,
-        TimepickerModule,
         TooltipModule,
         TypeaheadModule,
         CheckallDirective,
@@ -85,7 +75,7 @@ import { ScrollableDirective } from './directives/scrollable/scrollable.directiv
     ]
 })
 
-// https://github.com/ocombe/ng2-translate/issues/209
+
 export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
