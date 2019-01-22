2004a5373d28a53c196caca65dca2eed4b3992c1
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2019-01-22 19:19:14 +0200

    Changed selectes to dropdowns. Added panel style

--- src/ImageGallery.Client/wwwroot/app/routes/gallery/reports/reports.component.ts
+++ src/ImageGallery.Client/wwwroot/app/routes/gallery/reports/reports.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TitleService } from '../../../services/title.service';
 import { GalleryService } from '../../../gallery.service';
 import { NgxLoadingSpinnerService } from 'ngx-loading-spinner-fork';
+import { combineLatest } from 'rxjs/operators';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-reports',
@@ -30,36 +32,45 @@ export class ReportsComponent implements OnInit {
   getReports() {
     this.spinnerService.show();
 
-    this.galleryService.getReports('books')
-      .subscribe((res: any[]) => {
-        this.bookReports = res;
+    const books$ = this.galleryService.getReports('books');
+    const catalogs$ = this.galleryService.getReports('catalogs')
+    const references$ = this.galleryService.getReports('references');
+
+    zip(books$, catalogs$, references$,
+      (books, catalogs, references) => ({ books, catalogs, references }))
+      .subscribe((pair: any) => {
+        pair.books.forEach(book => {
+          book.downloadUrl = `${this.reportsUrl}?ReferenceId=${book.key}&ReportType=book`;
+        });
+        this.bookReports = pair.books;
+
+        pair.catalogs.forEach(catalog => {
+          catalog.downloadUrl = `${this.reportsUrl}?Catalog=${catalog.key}&ReportType=catalog`;
+        });
+        this.catalogReports = pair.catalogs;
+
+        pair.references.forEach(reference => {
+          reference.downloadUrl = `${this.reportsUrl}?ReferenceId=${reference.key}&ReportType=reference`;
+        });
+
+        this.referenceReports = pair.references;
         this.spinnerService.hide();
-      });
-    this.galleryService.getReports('catalogs')
-      .subscribe((res: any[]) => {
-        this.catalogReports = res;
-        this.spinnerService.hide();
-      });
-    this.galleryService.getReports('references')
-      .subscribe((res: any[]) => {
-        this.referenceReports = res;
-        this.spinnerService.hide();
-      });
+      })
   }
 
-  onSelectBook() {
-    var link = `${this.reportsUrl}?ReferenceId=${this.selectedReport.key}&ReportType=book`;
-    window.open(link, "_blank");
-  }
+  // onSelectBook() {
+  //   var link = 
+  //   window.open(link, "_blank");
+  // }
 
-  onSelectCatalog() {
-    var link = `${this.reportsUrl}?Catalog=${this.selectedReport.key}&ReportType=catalog`;
-    window.open(link, "_blank");
-  }
+  // onSelectCatalog() {
+  //   var link = `${this.reportsUrl}?Catalog=${this.selectedReport.key}&ReportType=catalog`;
+  //   window.open(link, "_blank");
+  // }
 
-  onSelectReference() {
-    var link = `${this.reportsUrl}?ReferenceId=${this.selectedReport.key}&ReportType=reference`;
-    window.open(link, "_blank");
-  }
+  // onSelectReference() {
+  //   var link = `${this.reportsUrl}?ReferenceId=${this.selectedReport.key}&ReportType=reference`;
+  //   window.open(link, "_blank");
+  // }
 
 }
